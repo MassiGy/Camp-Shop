bcc144b265392b97967042e60b4f8598c5ea5b47
MassiGy <ghernaoutmassi@gmail.com>
2021-04-28 15:16:25 +0000

    Styles: show page (details)

--- public/scripts/mapboxScripts.js
+++ public/scripts/mapboxScripts.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: campCoordinates, // starting position [lng, lat]
-    zoom: 4 // starting zoom
+    zoom: 2 // starting zoom
 });
 
 
