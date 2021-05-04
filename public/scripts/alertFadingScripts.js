929a2626302778f03ff89f3e81169387f0cd65a2
MassiGy <ghernaoutmassi@gmail.com>
2021-05-04 13:03:52 +0000

    Styles:login,Signup/out + Login Alert Tweak

--- public/scripts/alertFadingScripts.js
+++ public/scripts/alertFadingScripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $(".alert").hide();
     $(".alert").toggle(function showAlert() {
+        if (window.location.href.includes('/login')) {
+            $(".alert").fadeTo(7000, 500).slideUp(500, function() {
+                $(".alert").slideUp(500);
+            });
+            return $('.alert').append('<a href = "/Signup" > Not Signed Up Yet ? </a >')
+        }
         $(".alert").fadeTo(2000, 500).slideUp(500, function() {
             $(".alert").slideUp(500);
         });
+
     });
 });
