5e560015ddc1cc19a9da693dc29901d3649d58f3
MassiGy <ghernaoutmassi@gmail.com>
2021-05-04 13:50:35 +0000

    Trying to seed db

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async() => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 100; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
@@ -52,6 +52,7 @@ const seedDB = async() => {
     }
 }
 
-seedDB().then(() => {
-    mongoose.connection.close()
-})
+seedDB()
+    // .then(() => {
+    //     mongoose.connection.close()
+    // })
