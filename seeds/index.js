6f6249fdadb8f7009ded5c9b94160b2f5a014247
MassiGy <ghernaoutmassi@gmail.com>
2021-05-04 14:15:46 +0000

    Trying to seed db 4

--- seeds/index.js
+++ seeds/index.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose')
 const cities = require('./cities')
 const { descriptors, places } = require('./titles')
 const Campground = require('../modals/campground')
+const dbUrl = process.env.dataBaseUrl || 'mongodb://localhost:27017/myApp';
 
 
 
 
 
 
-mongoose.connect('mongodb://localhost:27017/myApp', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -25,11 +26,7 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 
 
 const seedDB = async() => {
-    // try {
-    //     await Campground.deleteMany({});
-    // } catch (e) {
-    //     console.log(e.message)
-    // }
+    await Campground.deleteMany({});
 
     for (let i = 0; i < 100; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
@@ -57,7 +54,6 @@ const seedDB = async() => {
     }
 }
 
-seedDB()
-    // .then(() => {
-    //     mongoose.connection.close()
-    // })
+seedDB().then(() => {
+    mongoose.connection.close()
+})
