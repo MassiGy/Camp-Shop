e7d9d4ddc7444fba48f73d26114a57dbef3505ba
MassiGy <ghernaoutmassi@gmail.com>
2023-05-22 12:19:45 +0200

    +dev-massigy: improved performance on review routes & fixed the delete review route.

--- routes/Controllers/reviewControllers.js
+++ routes/Controllers/reviewControllers.js
@@ -11,48 +11,41 @@ module.exports.showPage = (req, res) => {
     res.redirect(`/campgrounds/${id}`)
 }
 
-module.exports.postReview = async(req, res) => {
+module.exports.postReview = async (req, res) => {
     // validate the data
     dataValidator(reviewValidator, req.body)
-    
-    // get the campground using the url id
+
+    // get the campground  id from url
     const { id } = req.params;
-    const parentCamp = await Campground.findById(id)
-   
-    if (parentCamp) {
-        
-        // if any campground was found, add a review to it
-        const postedReview = new Review(req.body.review);
-        postedReview.owner = req.user._id;
-        parentCamp.reviews.unshift(postedReview)
-        
-        // save the campround & the review concurrently
-        await Promise.allSettled([
-            await postedReview.save(),
-            await parentCamp.save()
-        ])
-        
-        // inform the user
-        req.flash('success', 'Successfuly Posted Review!')
-        res.redirect(`/campgrounds/${id}`)
-   
-    } else {
-        
-        // if no campground found, inform user that review couldn't be added
-        req.flash('danger', 'Camp Not Found')
-        res.redirect('/campgrounds')
-   
-    }
+
+    // create a review from the form data
+    const postedReview = new Review(req.body.review);
+    postedReview.owner = req.user._id;
+
+
+    // modify the campround & the review concurrently
+    await Promise.allSettled([
+        postedReview.save(),
+        Campground.updateOne({ _id: id }, { $push: { reviews: postedReview._id } })
+    ]);
+
+    // inform the user
+    req.flash('success', 'Successfuly Posted Review!')
+    res.redirect(`/campgrounds/${id}`)
+
 }
 
-module.exports.delete = async(req, res) => {
+module.exports.delete = async (req, res) => {
 
     // get the ids from the url
     const { reviewId, id } = req.params;
-   
-    // delete the review if any, then redirect
-    await Review.findByIdAndDelete(reviewId)
-   
+
+    // delete the review & modify the parent camp concurrently
+    await Promise.allSettled([
+        Review.findByIdAndDelete(reviewId),
+        Campground.updateOne({ _id: id }, { $pull: { reviews: reviewId } })
+    ]);
+
     req.flash('success', 'Successfuly Deleted Review')
     res.redirect(`/campgrounds/${id}`);
 }
